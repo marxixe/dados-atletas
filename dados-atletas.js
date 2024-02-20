95f8ba537b762d524058829170627a0b7d8d2d82
Eduardo Andrade Marchi <krzputzdu7@gmail.com>
2024-02-20 20:50:18 -0300

    Primeiro commit

---
+++ dados-atletas.js
@@ -0,0 +1,85 @@
+class Atleta {
+    constructor(nome, idade, peso, altura, notas) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+        this.notas = notas;
+    };
+
+    calculaCategoria() {
+        if (this.idade >= 9 && this.idade <= 11) {
+            return "Infantil"
+        }
+        else if (this.idade <= 13) {
+            return "Juvenil"
+        }
+        else if (this.idade <= 15) {
+            return "Intermediário"
+        }
+        else if (this.idade <= 30) {
+            return "Adulto"
+        }
+        else {
+            return "Sem categoria"
+        }
+    };
+
+    calculaIMC() {
+        return this.peso / (this.altura * this.altura);
+    };
+
+    calculaMediaValida() {
+        for (let i = 0; i < this.notas.length; i++) {
+            this.notas.sort(function(a, b){
+                return a - b;
+              });
+            let notasComputadas = this.notas.slice(1, 4);
+            let soma = 0;
+            notasComputadas.forEach(function(nota) {
+                soma = soma + nota;
+              });
+              let media = soma / notasComputadas.length;
+              return media;
+        };
+    };
+
+    obtemNomeAtleta() {
+        return this.nome;
+    };
+
+    obtemIdadeAtleta() {
+        return this.idade;
+    };
+
+    obtemPesoAtleta() {
+        return this.peso;
+    };
+
+    obtemNotasAtleta() {
+        return this.notas;
+    };
+
+    obtemCategoria() {
+        return this.calculaCategoria();
+    };
+
+    obtemIMC() {
+        return this.calculaIMC();
+    };
+
+    obtemMediaValida() {
+        return this.calculaMediaValida();
+    };
+};
+
+const atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88]);
+
+console.log("Nome:", atleta.obtemNomeAtleta());
+console.log("Idade:", atleta.obtemIdadeAtleta());
+console.log("Peso:", atleta.obtemPesoAtleta());
+console.log("Altura:", atleta.altura);
+console.log("Notas:", atleta.obtemNotasAtleta().join(","));
+console.log("Categoria:", atleta.obtemCategoria());
+console.log("IMC:", atleta.obtemIMC());
+console.log("Média válida:", atleta.obtemMediaValida());
